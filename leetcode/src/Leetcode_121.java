b87c081ea5ee10ad99ebd32bcef56fa27f09601c
tudiiii <jhh0207@hanmail.net>
2020-08-29 17:12:27 +0900

    [LeetCode] 121. Best Time to Buy and Sell Stock

--- leetcode/src/Leetcode_121.java
+++ leetcode/src/Leetcode_121.java
@@ -1,12 +1,14 @@
 public class Leetcode_121 {
     public int maxProfit(int[] prices) {
 
+        int price = 0;
         int maxPrice = 0;
         int length = prices.length;
 
         for(int i=0;i<length;i++){
             for(int j=1;j<length;j++){
-                if(prices[j]-prices[i]>maxPrice && i<j){
+                price = prices[j]-prices[i];
+                if(price>maxPrice && i<j){
                     maxPrice = prices[j]-prices[i];
                 }
             }
