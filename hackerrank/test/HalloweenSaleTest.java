f010ceee6fb9034c24d56781b990a7583481b5c4
tudiiii <jhh0207@hanmail.net>
2020-09-12 21:42:13 +0900

    hackerrank. HallowwenSaleTest

---
+++ hackerrank/test/HalloweenSaleTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HalloweenSaleTest {
+
+    HalloweenSale s = new HalloweenSale();
+
+    @Test
+    public void test(){
+
+        Assert.assertEquals(s.howManyGames(20,3,6,80),6);
+        Assert.assertEquals(s.howManyGames(20,3,6,85),7);
+    }
+
+}
