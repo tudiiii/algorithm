b1acae3a61dbc08091fc312b3c29d88ade975837
tudy <jhh0207@hanmail.net>
2020-09-22 23:28:35 +0900

    Leetcode 21. Merge Two Sorted Lists ( Test 클래스 생성 및 문제 이해 )

---
+++ leetcode/src/Leetcode_21.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+public class Leetcode_21 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        LinkedList<Integer> list1;
+
+        System.out.println(l1.val);
+
+
+
+        return l1;
+    }
+}
