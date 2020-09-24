d3ec46636742537f342b5565e0d3f19cd8e20e89
tudy <jhh0207@hanmail.net>
2020-09-25 00:49:54 +0900

    Leetcode 104. Maximum Depth of Binary Tree

--- leetcode/src/Leetcode_104.java
+++ leetcode/src/Leetcode_104.java
@@ -1,3 +1,9 @@
+
+/*
+104. Maximum Depth of Binary Tree
+runtime 0 ms	memory 39.1 MB
+*/
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -20,7 +26,7 @@ public class Leetcode_104 {
         return getMaxDepth(root, 0);
     }
 
-    public int getMaxDepth(TreeNode root, int depth) {
+    public int getMaxDepth(TreeNode root, int depth) { // right vs left
         if (root == null) return depth;
 
         return Math.max(getMaxDepth(root.left, depth + 1), getMaxDepth(root.right, depth + 1));
