c78a1ecb6ce3ca95fa4b5169e91fe06bcccf6b2e
tudy <jhh0207@hanmail.net>
2020-09-29 23:49:30 +0900

    Leetcode 448. Find All Numbers Disappeared in an Array

--- leetcode/src/Leetcode_448.java
+++ leetcode/src/Leetcode_448.java
@@ -6,7 +6,7 @@ public class Leetcode_448 {
 
         HashSet<Integer> numList = new LinkedHashSet<Integer>(); // 중복제
         LinkedList<Integer> element = new LinkedList<>();
-        List<Integer> disappeareNum = new ArrayList<Integer>(); // 사라진 숫자
+        List<Integer> disappearNum = new ArrayList<Integer>(); // 사라진 숫자
 
         for(int num:nums) numList.add(num); // 중복제거된 값 hash -> LinkedList
         for(int num:numList) element.add(num); // LinkedList -> List
@@ -15,13 +15,13 @@ public class Leetcode_448 {
 
         while(i<=nums.length){ // nums length만큼 돌면서 사라진 숫자 찾기
             if (element.peek()==null || element.peek() != i) { // 값이 다르거나 null일 경우 숫자가 사라졌다는걸 알 수 있음
-                disappeareNum.add(i);
+                disappearNum.add(i);
             }else{
                 element.poll();
             }
             i++;
         }
 
-        return disappeareNum;
+        return disappearNum;
     }
 }
