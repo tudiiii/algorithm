1cb73e91028d86bbab04391bba0b91d786594179
tudy <jhh0207@hanmail.net>
2020-09-29 23:21:57 +0900

    Leetcode 448. Find All Numbers Disappeared in an Array [test case error...]

--- leetcode/src/Leetcode_448.java
+++ leetcode/src/Leetcode_448.java
@@ -1,8 +1,27 @@
-import java.util.List;
+import java.util.*;
 
 public class Leetcode_448 {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        
+        Arrays.sort(nums);
 
+        HashSet<Integer> numList = new LinkedHashSet<Integer>();
+        LinkedList<Integer> element = new LinkedList<>();
+        List<Integer> out = new ArrayList<Integer>();
+
+        for(int num:nums) numList.add(num);
+        for(int num:numList) element.add(num);
+
+        int i=1;
+        while(!element.isEmpty()){
+            System.out.println(element.peek());
+            if(element.peek()==i){
+                element.poll();
+            }else {
+                out.add(element.poll());
+            }
+            i++;
+        }
+
+        return out;
     }
 }
