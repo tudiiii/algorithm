d6b3bd74024faa10db42e525b6559a296aa97d33
tudy <jhh0207@hanmail.net>
2020-09-30 18:15:46 +0900

    Leetcode 448. Find All Numbers Disappeared in an Array [두번째푼방법]

--- leetcode/src/Leetcode_448.java
+++ leetcode/src/Leetcode_448.java
@@ -5,12 +5,39 @@ Memory Usage: 47.7 MB
 import java.util.*;
 
 public class Leetcode_448 {
+    /*
+        첫째 푼 방법
+        Run time : 17 ms
+        Memory Usage: 47.6 MB
+    */
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        Arrays.sort(nums); // 정
 
-        HashSet<Integer> numList = new LinkedHashSet<Integer>(); // 중복제
+        Set<Integer> set = new HashSet<>(); // 중복제거, 정렬
+        List<Integer> disappearNum = new ArrayList<>();
+
+        for(int num : nums) {
+            set.add(num);
+        }
+
+        for(int i = 1; i<= nums.length; i++) {
+            if(!set.contains(i)) // num 값을 포함하고 있지 않으면 사라진 숫자이기 때문에
+                disappearNum.add(i); // disappeareNum에 add();
+        }
+
+        return disappearNum;
+    }
+
+     /*
+        첫번 푼 방법
+        Run time : 31 ms
+        Memory Usage: 47.7 MB
+    */
+    public List<Integer> findDisappearedNumbers2(int[] nums) {
+        Arrays.sort(nums); // 정렬
+
+        HashSet<Integer> numList = new LinkedHashSet<>(); // 중복제
         LinkedList<Integer> element = new LinkedList<>();
-        List<Integer> disappearNum = new ArrayList<Integer>(); // 사라진 숫자
+        List<Integer> disappearNum = new ArrayList<>(); // 사라진 숫자
 
         for(int num:nums) numList.add(num); // 중복제거된 값 hash -> LinkedList
         for(int num:numList) element.add(num); // LinkedList -> List
