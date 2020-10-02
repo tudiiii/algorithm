e72a6bc4b684ac497d70d41aa52cfe684d07201a
tudy <jhh0207@hanmail.net>
2020-10-02 23:14:29 +0900

    Programmers 42883. 큰 수 만들기 [실패]

--- programmers/src/Programmers_42883.java
+++ programmers/src/Programmers_42883.java
@@ -3,9 +3,9 @@ import java.util.Stack;
 public class Programmers_42883 {
     public String solution(String number, int k) {
 
+        Stack<Character> stack = new Stack<>(); // 스택
+        char[] nums = number.toCharArray(); // number 를 하나씩 쪼개서 char
         char[] answer = new char[number.length() - k];
-        Stack<Character> stack = new Stack<>();
-        char[] nums = number.toCharArray();
 
         int deleteCnt = 0;
         int i=0;
@@ -18,6 +18,7 @@ public class Programmers_42883 {
             }
 
             stack.push(nums[i]);
+
             i++;
             System.out.println(stack);
         }
