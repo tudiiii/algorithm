dc0a948cf65f7aa0ecfec592fe5cb6cb2c4bf3fc
tudy <jhh0207@hanmail.net>
2020-10-03 18:20:33 +0900

    Leetcode 1. Two Sum

---
+++ leetcode/src/Leetcode_1.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leetcode_1 {
+    public int[] twoSum(int[] nums, int target) {
+
+        List<Integer> answerList = new ArrayList<>();
+        int count;
+
+        for(int i=0;i<nums.length;i++){
+            int num = target-nums[i];
+            count=0;
+
+            while(count!=nums.length){
+                if(num == nums[count] && count!=i){
+                    answerList.add(i);
+                    answerList.add(count);
+                    System.out.println(answerList.size());
+                    break;
+                }
+                count++;
+            }
+
+            if(answerList.size()==2) break;
+        }
+
+        int[] answer = new int[answerList.size()];
+        for(int i=0; i<answerList.size(); i++){
+            answer[i] = answerList.get(i);
+        }
+
+        return answer;
+    }
+}
