c2b7aa8f70e95785e297570ac1ed708f13957747
tudy <jhh0207@hanmail.net>
2020-10-05 23:58:17 +0900

    Leetcode 617. Merge Two Binary Trees [problem + test case]

---
+++ leetcode/src/Leetcode_617.java
@@ -0,0 +1,20 @@
+
+  public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }*
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+public class Leetcode_617 {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+
+        return t1;
+    }
+}
