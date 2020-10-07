9aceee0b17676da3ed85b0cdddf09a0d7c659d46
tudy <jhh0207@hanmail.net>
2020-10-07 22:59:28 +0900

    617. Merge Two Binary Trees

--- leetcode/src/Leetcode_617.java
+++ leetcode/src/Leetcode_617.java
@@ -11,11 +11,23 @@ class TreeNode2 {
           this.right = right;
       }
   }
-
+/*
+Runtime: 0 ms
+Memory Usage: 39.1 MB
+ */
 public class Leetcode_617 {
     public TreeNode2 mergeTrees(TreeNode2 t1, TreeNode2 t2) {
 
+        TreeNode2 mergeTree = new TreeNode2();
 
-        return t1;
+        if(t1==null && t2==null) return null;
+        else if(t1==null) return t2;
+        else if(t2==null) return t1;
+        else mergeTree.val= t1.val+t2.val;
+
+        mergeTree.left = mergeTrees(t1.left, t2.left);
+        mergeTree.right = mergeTrees(t1.right, t2.right);
+
+        return mergeTree;
     }
 }
