2613b080c09bc839a611ec7c940c1d0b70cd554e
tudy <jhh0207@hanmail.net>
2020-10-24 23:54:40 +0900

    Programmers 17680. 캐시

--- programmers/src/Programmers_17680.java
+++ programmers/src/Programmers_17680.java
@@ -1,25 +1,25 @@
 /*
-[1차] 캐시
-테스트 1 〉	통과 (0.13ms, 52.2MB)
-테스트 2 〉	통과 (0.14ms, 52.8MB)
-테스트 3 〉	통과 (0.18ms, 53.2MB)
-테스트 4 〉	통과 (0.16ms, 52.3MB)
-테스트 5 〉	통과 (0.16ms, 53.2MB)
-테스트 6 〉	통과 (0.07ms, 52.6MB)
-테스트 7 〉	통과 (0.08ms, 52.1MB)
-테스트 8 〉	통과 (0.17ms, 52.5MB)
-테스트 9 〉	통과 (0.11ms, 52.4MB)
-테스트 10 〉	통과 (0.61ms, 52.2MB)
-테스트 11 〉	실패 (90.17ms, 83.8MB)
-테스트 12 〉	통과 (0.55ms, 53MB)
-테스트 13 〉	통과 (1.05ms, 52.6MB)
-테스트 14 〉	통과 (1.44ms, 52.5MB)
-테스트 15 〉	실패 (2.01ms, 52.1MB)
-테스트 16 〉	통과 (2.15ms, 52.1MB)
-테스트 17 〉	통과 (0.08ms, 54.1MB)
-테스트 18 〉	실패 (4.64ms, 54.3MB)
-테스트 19 〉	실패 (4.49ms, 52.8MB)
-테스트 20 〉	실패 (4.23ms, 53.1MB)
+[1차] 캐
+테스트 1 〉	통과 (0.16ms, 51.9MB)
+테스트 2 〉	통과 (0.16ms, 52MB)
+테스트 3 〉	통과 (0.16ms, 52.3MB)
+테스트 4 〉	통과 (0.19ms, 53.1MB)
+테스트 5 〉	통과 (0.15ms, 53MB)
+테스트 6 〉	통과 (0.06ms, 52.2MB)
+테스트 7 〉	통과 (0.08ms, 52.3MB)
+테스트 8 〉	통과 (0.20ms, 52.9MB)
+테스트 9 〉	통과 (0.16ms, 51.7MB)
+테스트 10 〉	통과 (0.42ms, 52.5MB)
+테스트 11 〉	통과 (57.42ms, 83.6MB)
+테스트 12 〉	통과 (0.40ms, 52.7MB)
+테스트 13 〉	통과 (0.62ms, 53.1MB)
+테스트 14 〉	통과 (1.03ms, 52.8MB)
+테스트 15 〉	통과 (1.34ms, 52.8MB)
+테스트 16 〉	통과 (1.64ms, 53.1MB)
+테스트 17 〉	통과 (0.10ms, 53.1MB)
+테스트 18 〉	통과 (3.46ms, 52.7MB)
+테스트 19 〉	통과 (3.99ms, 53.5MB)
+테스트 20 〉	통과 (3.06ms, 52.6MB)
  */
 
 import java.util.LinkedList;
@@ -27,33 +27,27 @@ import java.util.Queue;
 
 public class Programmers_17680 {
     public int solution(int cacheSize, String[] cities) {
-        //2, new String[]{"Jeju", "Pangyo", "NewYork", "newyork"}),16)
-        //"Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"
         Queue<String> cache = new LinkedList< >();
 
-        int size = 0;
-        int count = 0;
-
+        int answer = 0;
         if(cacheSize==0) return cities.length * 5;
 
         for(String city : cities){
-            if(cache.contains(city.toLowerCase())) {
-                count+= 1;
+            city = city.toLowerCase();
+            if(cache.contains(city)) {
+                answer+= 1;
+                cache.remove(city);
             }else{
+                answer+= 5;
 
-                if(size==cacheSize){
+                if(cache.size()>=cacheSize){
                     cache.poll();
-                    size--;
                 }
-
-                count+= 5;
-                cache.add(city.toLowerCase());
-                size++;
             }
-            System.out.println(cache);
+            cache.add(city);
         }
 
-        return count;
+        return answer;
     }
 }
 
