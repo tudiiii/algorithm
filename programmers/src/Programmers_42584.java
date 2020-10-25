a1feec0676ddca9f61e6a29ce4db7acd0ce07d56
tudy <jhh0207@hanmail.net>
2020-10-25 23:55:43 +0900

    Programmers 42584. 주식가격 [문제생성]

---
+++ programmers/src/Programmers_42584.java
@@ -0,0 +1,6 @@
+public class Programmers_42584 {
+    public int[] solution(int[] ints, int[] prices) {
+        int[] answer = {};
+        return answer;
+    }
+}
---
+++ programmers/test/Programmers_42584Test.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Programmers_42584Test {
+
+    Programmers_42584 s = new Programmers_42584();
+
+    @Test
+    public void test(){
+        s.solution(new int[]{1, 2, 3, 2, 3},new int[]{4, 3, 1, 1, 0});
+    }
+
+}
