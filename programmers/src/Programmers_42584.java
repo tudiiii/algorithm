956159cd161c847a8bd661c0c6270c5528273cd9
tudy <jhh0207@hanmail.net>
2020-10-27 23:44:28 +0900

    Programmers 42584. 주식가격 [ing]

--- programmers/src/Programmers_42584.java
+++ programmers/src/Programmers_42584.java
@@ -1,6 +1,23 @@
+import java.util.Stack;
+
 public class Programmers_42584 {
-    public int[] solution(int[] ints, int[] prices) {
-        int[] answer = {};
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        Stack<Integer> priceStack = new Stack<>();
+
+        for(int price : prices) priceStack.push(price);
+
+        for(int i=0;i<prices.length;i++){
+            int priceTop = priceStack.pop();
+            int count = 0;
+            while(priceTop>priceStack.peek()){
+                count++;
+            }
+            answer[prices.length-1] = count;
+        }
+
+
         return answer;
     }
 }
