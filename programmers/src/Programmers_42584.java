41c06cabefe3afe1089f21e6fdf2c0d5efafda16
tudy <jhh0207@hanmail.net>
2020-10-28 23:43:26 +0900

    Programmers 42584. 주식가격 [실패]

--- programmers/src/Programmers_42584.java
+++ programmers/src/Programmers_42584.java
@@ -1,22 +1,37 @@
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class Programmers_42584 {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
+        List<Integer> answerList = new ArrayList<>();
 
-        Stack<Integer> priceStack = new Stack<>();
+        Queue<Integer> priceQueue = new LinkedList<>();
 
-        for(int price : prices) priceStack.push(price);
+        for(int price : prices) priceQueue.add(price);
 
-        for(int i=0;i<prices.length;i++){
-            int priceTop = priceStack.pop();
-            int count = 0;
-            while(priceTop>priceStack.peek()){
-                count++;
+        int count;
+        int index = 1;
+        while(!priceQueue.isEmpty()){
+            count = 0;
+            for(int i=index;i<prices.length;i++){
+                System.out.println(priceQueue.peek() + ", " + prices[i]);
+                if(priceQueue.peek()<prices[i]) {
+                    count++;
+                }
             }
-            answer[prices.length-1] = count;
+            priceQueue.poll();
+
+            answerList.add(count);
+            System.out.println(answerList);
+            index++;
         }
 
+        for(int i=0;i<answerList.size();i++){
+            answer[i] = answerList.get(i);
+        }
 
         return answer;
     }
