00b13de1f81aab44ada5dabc0eedfef7faf1b63c
tudy <jhh0207@hanmail.net>
2020-10-29 23:42:36 +0900

    Programmers 42584. 주식가격

--- programmers/src/Programmers_42584.java
+++ programmers/src/Programmers_42584.java
@@ -1,36 +1,36 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-
+/* 주식가격
+정확성  테스트
+테스트 1 〉	통과 (0.02ms, 52.6MB)
+테스트 2 〉	통과 (0.03ms, 52.4MB)
+테스트 3 〉	통과 (0.18ms, 53.1MB)
+테스트 4 〉	통과 (0.20ms, 53MB)
+테스트 5 〉	통과 (0.17ms, 54.4MB)
+테스트 6 〉	통과 (0.03ms, 52.7MB)
+테스트 7 〉	통과 (0.12ms, 52.3MB)
+테스트 8 〉	통과 (0.15ms, 53.1MB)
+테스트 9 〉	통과 (0.02ms, 52.4MB)
+테스트 10 〉	통과 (0.25ms, 52.7MB)
+효율성  테스트
+테스트 1 〉	통과 (14.28ms, 71MB)
+테스트 2 〉	통과 (13.70ms, 66.5MB)
+테스트 3 〉	통과 (13.67ms, 75.1MB)
+테스트 4 〉	통과 (14.01ms, 71.5MB)
+테스트 5 〉	통과 (9.47ms, 64.4MB)
+ */
 public class Programmers_42584 {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        List<Integer> answerList = new ArrayList<>();
 
-        Queue<Integer> priceQueue = new LinkedList<>();
+        int count = 0; // 시간 count
 
-        for(int price : prices) priceQueue.add(price);
-
-        int count;
-        int index = 1;
-        while(!priceQueue.isEmpty()){
-            count = 0;
-            for(int i=index;i<prices.length;i++){
-                System.out.println(priceQueue.peek() + ", " + prices[i]);
-                if(priceQueue.peek()<prices[i]) {
-                    count++;
-                }
+        for(int i=0;i<prices.length;i++){
+            for (int j = i+1; j < prices.length; j++) {
+                count++;
+                if (prices[i] > prices[j])  break; // 주식가격이 더 크면 break
             }
-            priceQueue.poll();
 
-            answerList.add(count);
-            System.out.println(answerList);
-            index++;
-        }
-
-        for(int i=0;i<answerList.size();i++){
-            answer[i] = answerList.get(i);
+            answer[i] = count;
+            count = 0;
         }
 
         return answer;
--- programmers/test/Programmers_42584Test.java
+++ programmers/test/Programmers_42584Test.java
@@ -7,7 +7,7 @@ public class Programmers_42584Test {
 
     @Test
     public void test(){
-        //Assert.assertEquals(s.solution(new int[]{1, 2, 3, 2, 3}),new int[]{4, 3, 1, 1, 0});
+        Assert.assertEquals(s.solution(new int[]{1, 2, 3, 2, 3}),new int[]{4, 3, 1, 1, 0});
         Assert.assertEquals(s.solution(new int[]{3,7,4,2,1}),new int[]{2,1,2,1,0});
     }
 
