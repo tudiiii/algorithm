ea03d0a6cb374bacdd80bfcfbcc3a498e129459c
tudy <jhh0207@hanmail.net>
2020-10-31 14:23:52 +0900

    Programmers 42895. n으로 표현

--- programmers/src/N으로표현/Programmers_42895.java
+++ programmers/src/N으로표현/Programmers_42895.java
@@ -1,8 +1,35 @@
 package N으로표현;
 
 public class Programmers_42895 {
+
+    static int num = -1;
     public int solution(int N, int number) {
-        int answer = 0;
-        return answer;
+
+        calculation(N,number,0,0);
+
+        return num;
+    }
+
+    public void calculation(int N, int number,int count, int result){
+
+        int NN = N;
+
+        if(count>8){
+            num = -1;
+            return;
+        }
+        if(result==number){
+            if(num== -1 || num >count)
+                num = count;
+            return;
+        }
+
+        for(int i=1;i<9-count;i++){
+            calculation(N,number,count+i,result+NN);
+            calculation(N,number,count+i,result-NN);
+            calculation(N,number,count+i,result*NN);
+            calculation(N,number,count+i,result/NN);
+            NN = NN * 10 + N;
+        }
     }
 }
