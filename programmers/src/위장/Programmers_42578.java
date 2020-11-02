be23cd1ddeed10ca990d84b6a5ad88e82c73d2ac
tudy <jhh0207@hanmail.net>
2020-11-02 23:59:11 +0900

    Programmers 42578. 위장 [문제생성]

---
+++ programmers/src/위장/Programmers_42578.java
@@ -0,0 +1,8 @@
+package 위장;
+
+public class Programmers_42578 {
+    public int solution(String[][] clothes) {
+        int answer = 0;
+        return answer;
+    }
+}
---
+++ programmers/test/위장/Programmers_42578Test.java
@@ -0,0 +1,13 @@
+package 위장;
+
+import org.junit.Test;
+
+public class Programmers_42578Test {
+
+    Programmers_42578 s = new Programmers_42578();
+
+    @Test
+    public void test(){
+    }
+
+}
