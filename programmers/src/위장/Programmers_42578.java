57e28a98d3867d29962886b7f7953e508928e9be
tudy <jhh0207@hanmail.net>
2020-11-04 23:15:38 +0900

    Programmers 42578. 위장 [ing]

--- programmers/src/위장/Programmers_42578.java
+++ programmers/src/위장/Programmers_42578.java
@@ -1,8 +1,22 @@
 package 위장;
 
+import java.util.HashMap;
+
 public class Programmers_42578 {
     public int solution(String[][] clothes) {
         int answer = 0;
+        //[[yellow_hat, headgear], [blue_sunglasses, eyewear], [green_turban, headgear]]
+        HashMap<String, String> clothesHash = new HashMap<>();
+
+        clothesHash.put("yellow_hat","headgear");
+        clothesHash.put("blue_sunglasses","eyewear");
+        clothesHash.put("green_turban","headgear");
+
+        int count = 0;
+        for(int i=0;i<clothesHash.size();i++){
+            if()
+        }
+
         return answer;
     }
 }
