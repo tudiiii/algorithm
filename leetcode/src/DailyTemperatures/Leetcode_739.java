efa40189c0497c2857361e960a7f433ea59808e8
tudy <jhh0207@hanmail.net>
2020-11-26 23:18:05 +0900

    Leetcode 739. Daily Temperatures

--- leetcode/src/DailyTemperatures/Leetcode_739.java
+++ leetcode/src/DailyTemperatures/Leetcode_739.java
@@ -1,12 +1,30 @@
-/*
-Runtime: 1081 ms
-Memory Usage: 47.6 MB
- */
-
 package DailyTemperatures;
 
+import java.util.Stack;
+
 public class Leetcode_739 {
+    /*
+    Runtime: 13 ms
+    Memory Usage: 47.6 MB
+     */
     public int[] dailyTemperatures(int[] T) {
+        Stack<Integer> stack = new Stack<>();
+        int[] answer = new int[T.length];
+        for(int i = 0; i < T.length; i++) {
+            while(!stack.isEmpty() && T[i] > T[stack.peek()]) {
+                int idx = stack.pop();
+                answer[idx] = i - idx;
+            }
+            stack.push(i);
+        }
+        return answer;
+    }
+
+    /*
+    Runtime: 1081 ms
+    Memory Usage: 47.6 MB
+     */
+    public int[] dailyTemperatures2(int[] T) {
         int[] answer = new int[T.length];
 
         int count = 0;
@@ -20,7 +38,6 @@ public class Leetcode_739 {
             }
             count = 0;
         }
-
         return answer;
     }
 }
