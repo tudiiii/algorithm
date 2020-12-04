1a507d6a1654b305f6d94023bcbeba8ac6d383bd
tudy <jhh0207@hanmail.net>
2020-12-04 23:24:42 +0900

    Programmers 42628. 이중우선순위큐 [ 테스트케이스 불통 ]

--- programmers/src/이중우선순위큐/Programmers_42628.java
+++ programmers/src/이중우선순위큐/Programmers_42628.java
@@ -1,8 +1,29 @@
 package 이중우선순위큐;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class Programmers_42628 {
     public int[] solution(String[] operations) {
-        int[] answer = {};
-        return answer;
+        Deque<Integer> operationDeque = new ArrayDeque<>();
+
+        for(int i=0;i<operations.length;i++){
+            String[] operation = operations[i].split(" ");
+            if("I".equals(operation[0])){
+                operationDeque.add(Integer.valueOf(operation[1]));
+            }else{
+                if("1".equals(operation[1])){
+                    operationDeque.removeFirst();
+                }else{
+                    operationDeque.removeLast();
+                }
+            }
+        }
+
+        if(operationDeque.isEmpty()){
+            return new int[]{0,0};
+        }
+
+        return new int[]{operationDeque.removeFirst(),operationDeque.removeLast()};
     }
 }
