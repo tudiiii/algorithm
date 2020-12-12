91958a5da269e64784d0d874df6ab531c28fcc0d
tudy <jhh0207@hanmail.net>
2020-12-12 20:46:27 +0900

    Leetcode 75. Sort Colors

--- leetcode/src/SortColors/Leetcode_75.java
+++ leetcode/src/SortColors/Leetcode_75.java
@@ -1,9 +1,26 @@
 package SortColors;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Leetcode_75 {
+    /*
+    Runtime: 0 ms
+    Memory Usage: 37.6 MB
+     */
     public void sortColors(int[] nums) {
 
-        return;
+        List<Integer> answerList = new ArrayList<>();
+
+        for(int j=0;j<3;j++){
+            for (int num : nums) { // 0,1,2 같은 값이 있을 경우 갯수만 세서 리스트에 저
+                if (num == j) answerList.add(j);
+            }
+        }
+
+        for(int i=0;i<answerList.size();i++){
+            nums[i] = answerList.get(i);
+        }
 
     }
 }
