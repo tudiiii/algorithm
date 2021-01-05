23aace0e0408aeaa4ea29ed5aefedcb331953b2a
tudy <jhh0207@hanmail.net>
2021-01-05 23:42:58 +0900

    Programmers 42891. 무지의 먹방라이브

---
+++ programmers/src/무지의먹방라이브/Programmers_42891.java
@@ -0,0 +1,13 @@
+package 무지의먹방라이브;
+
+public class Programmers_42891 {
+    public int solution(int[] food_times, long k) {
+        int answer = 0;
+
+        int sum = 0;
+        for(int i=0;i<food_times.length;i++){
+            sum += food_times[i];
+        }
+        return answer;
+    }
+}
