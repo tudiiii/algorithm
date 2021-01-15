83fba9e7c64e06273bad5125f5058656fb58945a
tudy <jhh0207@hanmail.net>
2021-01-15 21:04:28 +0900

    Programmers 60057. 문자열압축 [ing]

--- programmers/src/문자열압축/Programmers_60057.java
+++ programmers/src/문자열압축/Programmers_60057.java
@@ -2,7 +2,38 @@ package 문자열압축;
 
 public class Programmers_60057 {
     public int solution(String s) {
-        int answer = 0;
+        int answer = s.length();
+
+        for(int i=0;i<=s.length()/2;++i){
+            int pos = 0;
+            int len = s.length();
+
+            for( ; pos+i <= s.length() ;){
+                String unit = s.substring(pos, pos + i);
+                pos += i;
+
+                int count = 0;
+                for ( ; pos+i <= s.length() ; ){
+                    if(unit.equals(s.substring(pos,pos+i))){
+                        ++count;
+                        pos += i;
+                    }else{
+                        break;
+                    }
+                }
+
+                if(count > 0){
+                    len -= i * count;
+
+                    if(count < 9) len += 1;
+                    else if(count < 99) len += 2;
+                    else if(count < 999) len += 3;
+                    else len += 4;
+                }
+            }
+            answer = Math.min(answer,len);
+        }
+
         return answer;
     }
 }
