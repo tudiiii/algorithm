64820ad4651c050b3cbcc8d74637fbf017a142dd
tudy <jhh0207@hanmail.net>
2021-01-24 00:54:24 +0900

    Leetcode 394. decode string [testcase [여러번나오는경우 실패]

--- leetcode/src/DecodeString/Leetcode_394.java
+++ leetcode/src/DecodeString/Leetcode_394.java
@@ -2,23 +2,50 @@ package DecodeString;
 
 public class Leetcode_394 {
     public String decodeString(String s) {
-//        "3[a]2[bc]"),"aaabcbc"
-
+//        ("3[a2[c]]"),"accaccacc");
 
         StringBuilder sb = new StringBuilder();
         StringBuilder str = new StringBuilder();
         boolean flag = false;
+        boolean isNumber = false;
         int count = 0;
 
         for(int i=0;i<s.length();i++){
-            System.out.println("i " + i);
             char c = s.charAt(i);
 
            if(c >= '0' && c <= '9') {
-                count = c - '0';
+               if(isNumber && !flag){
+                   count = count * 10 + c - '0';
+               }
+               if(!isNumber && !flag){
+                   count = c - '0';
+               }
+               isNumber = true;
             }
            else if(c == '['){
+               if(flag){
+                   int z = i;
+                   int tmp = s.charAt(z-1) - '0';
+                   StringBuilder re = new StringBuilder();
+                   while(z<s.length()){
+                       if(s.charAt(z)==']') {
+                           break;
+                       }
+                       if(s.charAt(z)!='['){
+                           re.append(s.charAt(z));
+                       }
+                       z++;
+                   }
+
+                   for(int j=0;j<tmp;j++){
+                       System.out.println(j);
+                       str.append(re);
+                   }
+                   i = z+re.length()-1;
+                   System.out.println("i---- " + i);
+               }
                flag=true;
+               isNumber=false;
            }else if(c==']'){
                flag=false;
                for(int j=0;j<count;j++){
@@ -34,8 +61,8 @@ public class Leetcode_394 {
                 }
             }
 
+            System.out.println("count " + count);
         }
-
         System.out.println("sb " + sb);
         return sb.toString();
 
