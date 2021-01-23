1be6a4114ad450e2cfe023cfcb7a1ccfca2575f1
tudy <jhh0207@hanmail.net>
2021-01-23 23:49:23 +0900

    Leetcode 394. decode string [testcase 한개불통]

--- leetcode/src/DecodeString/Leetcode_394.java
+++ leetcode/src/DecodeString/Leetcode_394.java
@@ -2,6 +2,42 @@ package DecodeString;
 
 public class Leetcode_394 {
     public String decodeString(String s) {
+//        "3[a]2[bc]"),"aaabcbc"
+
+
+        StringBuilder sb = new StringBuilder();
+        StringBuilder str = new StringBuilder();
+        boolean flag = false;
+        int count = 0;
+
+        for(int i=0;i<s.length();i++){
+            System.out.println("i " + i);
+            char c = s.charAt(i);
+
+           if(c >= '0' && c <= '9') {
+                count = c - '0';
+            }
+           else if(c == '['){
+               flag=true;
+           }else if(c==']'){
+               flag=false;
+               for(int j=0;j<count;j++){
+                   sb.append(str);
+               }
+               str.setLength(0);
+           }
+            else{
+                if(flag){
+                    str.append(c);
+                }else{
+                    sb.append(c);
+                }
+            }
+
+        }
+
+        System.out.println("sb " + sb);
+        return sb.toString();
 
     }
 }
