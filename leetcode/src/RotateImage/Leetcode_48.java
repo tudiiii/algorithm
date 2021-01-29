6d9a5c786135cfeaed6dd93eb2dee146cdd28f8c
tudy <jhh0207@hanmail.net>
2021-01-29 23:56:54 +0900

    Leetcode 48. Rotate Image

--- leetcode/src/RotateImage/Leetcode_48.java
+++ leetcode/src/RotateImage/Leetcode_48.java
@@ -1,8 +1,18 @@
 package RotateImage;
 
 public class Leetcode_48 {
-    public int[][] rotate(int[][] matrix) {
+    public void rotate(int[][] matrix) {
 
-        return new int[][]{{1},{1}};
+        int l = matrix.length;
+        for (int i = 0; i < (l+1)/2; i ++) {
+            for (int j = 0; j < l/2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[l-1-j][i];
+                matrix[l-1-j][i] = matrix[l-1-i][l-1-j];
+                matrix[l-1-i][l-1-j] = matrix[j][l-1-i];
+                matrix[j][l-1-i] = temp;
+            }
+        }
     }
 }
+
