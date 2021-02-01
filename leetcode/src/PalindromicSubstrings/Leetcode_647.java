cbbbb8e613a4f870e935727fe75d96f864621a7f
tudy <jhh0207@hanmail.net>
2021-02-01 22:51:50 +0900

    Leetcode 647. Palindromic Substrings [문제&테스트케이스 생성]

---
+++ leetcode/src/PalindromicSubstrings/Leetcode_647.java
@@ -0,0 +1,8 @@
+package PalindromicSubstrings;
+
+public class Leetcode_647 {
+    public int countSubstrings(String s) {
+
+        return s.length();
+    }
+}
---
+++ leetcode/test/PalindromicSubstrings/Leetcode_647Test.java
@@ -0,0 +1,17 @@
+package PalindromicSubstrings;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Leetcode_647Test {
+    Leetcode_647 s = new Leetcode_647();
+
+    @Test
+    public void test(){
+        Assert.assertEquals(s.countSubstrings("abc"),3);
+        Assert.assertEquals(s.countSubstrings("aaa"),6);
+    }
+
+}
