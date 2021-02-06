0bbb9d333803daaa49810c654ea6bdf545226676
tudy <jhh0207@hanmail.net>
2021-02-07 01:02:56 +0900

    Leetcode 494. find targer sum ways

--- leetcode/src/TargetSum/Leetcode_494.java
+++ leetcode/src/TargetSum/Leetcode_494.java
@@ -1,8 +1,20 @@
 package TargetSum;
 
 public class Leetcode_494 {
+    int count = 0;
+
     public int findTargetSumWays(int[] nums, int S) {
 
-        return 1;
+        checkSymbol(nums, S, 0, 0);
+        return count;
+    }
+
+    public void checkSymbol(int[] nums, int S , int sum ,int l){
+        if (l != nums.length) {
+            checkSymbol(nums,S,sum+nums[l],l+1);
+            checkSymbol(nums,S,sum-nums[l],l+1);
+        } else {
+            if(sum == S) count++;
+        }
     }
 }
