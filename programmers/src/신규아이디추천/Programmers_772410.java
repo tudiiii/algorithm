d4f5f2ba4be50057f1bc3abc019ef346a18e87fa
tudy <jhh0207@hanmail.net>
2021-02-11 23:55:36 +0900

    Programmers 772410. 신규 아이디 추천 [문제 & 테스트 케이스 생성]

---
+++ programmers/src/신규아이디추천/Programmers_772410.java
@@ -0,0 +1,8 @@
+package 신규아이디추천;
+
+public class Programmers_772410 {
+    public String solution(String new_id) {
+        String answer = "";
+        return answer;
+    }
+}
---
+++ programmers/test/신규아이디추천/Programmers_772410Test.java
@@ -0,0 +1,21 @@
+package 신규아이디추천;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Programmers_772410Test {
+
+    Programmers_772410 s = new Programmers_772410();
+
+    @Test
+    public void test(){
+        Assert.assertEquals(s.solution("...!@BaT#*..y.abcdefghijklm"),"bat.y.abcdefghi");
+        Assert.assertEquals(s.solution("z-+.^."),"z--");
+        Assert.assertEquals(s.solution("=.="),"aaa");
+        Assert.assertEquals(s.solution("123_.def"),"123_.def");
+        Assert.assertEquals(s.solution("abcdefghijklmn.p"),"abcdefghijklmn");
+    }
+
+}
