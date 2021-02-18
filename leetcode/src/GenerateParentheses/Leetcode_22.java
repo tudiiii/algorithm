745acd42465bf4997f53f28baa36777fb4055879
tudy <jhh0207@hanmail.net>
2021-02-18 23:47:16 +0900

    Leetcode 22. Generate Parentheses

--- leetcode/src/GenerateParentheses/Leetcode_22.java
+++ leetcode/src/GenerateParentheses/Leetcode_22.java
@@ -1,14 +1,27 @@
 package GenerateParentheses;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Leetcode_22 {
     public List<String> generateParenthesis(int n) {
-        List<String> list = null;
+        List<String> list = new ArrayList<>();
 
-
-
+        backTrack(list, "", 0, 0, n);
 
         return list;
+    }
+
+    public void backTrack(List<String> list, String str, int open, int close, int max){
+        if(str.length() == max*2){
+            list.add(str);
+            return;
+        }
+
+        if(open < max) // n 크기보다  '(' 괄호수 작을경우
+            backTrack(list,str+"(", open+1, close, max);
+
+        if(open > close) // '(' 개수 > ')' 개수
+            backTrack(list, str+")", open, close+1, max);
     }
 }
--- leetcode/test/GenerateParentheses/Leetcode_22Test.java
+++ leetcode/test/GenerateParentheses/Leetcode_22Test.java
@@ -1,16 +1,17 @@
 package GenerateParentheses;
 
-import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
 
 public class Leetcode_22Test {
     Leetcode_22 s = new Leetcode_22();
 
     @Test
     public void test(){
-        Assert.assertEquals(s.generateParenthesis(3), new String[]{"((()))", "(()())", "(())()", "()(())", "()()()"});
+        assertEquals(s.generateParenthesis(3), asList("((()))","(()())","(())()","()(())","()()()"));
+        assertEquals(s.generateParenthesis(1), asList("()"));
     }
 
 }
