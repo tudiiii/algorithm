32a8ba8792cea895f2977d989abec3da0daf6f8c
tudy <jhh0207@hanmail.net>
2021-04-11 23:20:15 +0900

    Programmers 42839. 소수찾기

--- programmers/src/소수찾기/Programmers_42839.java
+++ programmers/src/소수찾기/Programmers_42839.java
@@ -1,8 +1,45 @@
 package 소수찾기;
 
+import java.util.HashSet;
+
 public class Programmers_42839 {
     public int solution(String numbers) {
-        int answer = 0;
-        return answer;
+        HashSet<Integer> set = new HashSet<>();
+
+        perm("",numbers, set); // 순열
+
+        int count = 0;
+        while(set.iterator().hasNext()){
+            int a = set.iterator().next();
+            set.remove(a);
+            if(a==2) count++;
+            if(a%2!=0 && isPrime(a)){
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void perm(String prefix, String str, HashSet<Integer> set){
+        int n = str.length();
+
+        if(!prefix.equals("")){
+            set.add(Integer.valueOf(prefix));
+        }
+
+        for ( int i=0 ; i < n ; i++ ){
+            perm(prefix+str.charAt(i),str.substring(0, i) + str.substring(i+1,n),set);
+        }
+    }
+
+    public boolean isPrime(int n){
+        if(n==0 || n==1) return false;
+
+        for(int i=2 ; i<Math.sqrt(n) ; i+=i){
+            if(n%i==0) return false;
+        }
+
+        return true;
     }
 }
