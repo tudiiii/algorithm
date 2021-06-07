463e258aa58063c49471ceb57ab27976bddf52ee
tudy <jhh0207@hanmail.net>
2021-06-07 22:45:13 +0900

    Leetcode 78. Subsets 문제 생성

---
+++ leetcode/src/Subsets/Leetcode_78.java
@@ -0,0 +1,12 @@
+package Subsets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Leetcode_78 {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+
+        return list;
+    }
+}
---
+++ leetcode/test/Subsets/Leetcode_78Test.java
@@ -0,0 +1,12 @@
+package Subsets;
+
+import org.junit.Test;
+
+public class Leetcode_78Test {
+    Leetcode_78 s = new Leetcode_78();
+
+    @Test
+    public void test(){
+
+    }
+}
