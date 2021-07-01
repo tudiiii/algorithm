3d5b72ec7ac6ebcca5e951ed1cf5911cb13968b2
tudy <jhh0207@hanmail.net>
2021-07-01 23:02:52 +0900

    Leetcode 53. maximum subarray

--- leetcode/src/MaximumSubarray/Leetcode_53.java
+++ leetcode/src/MaximumSubarray/Leetcode_53.java
@@ -1,8 +1,17 @@
 package MaximumSubarray;
 
+// kadane algorithm
 public class Leetcode_53 {
     public int maxSubArray(int[] nums) {
 
-        return 1;
+        int maxSum = nums[0];
+        int tmpSum = nums[0];
+
+        for(int i=1;i<nums.length;i++){
+            tmpSum = Math.max(tmpSum+nums[i], nums[i]);
+            maxSum = Math.max(maxSum, tmpSum);
+        }
+
+        return maxSum;
     }
 }
--- leetcode/src/Subsets/Leetcode_78.java
+++ leetcode/src/Subsets/Leetcode_78.java
@@ -1,12 +1,10 @@
 package Subsets;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Leetcode_78 {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
-
-        return list;
-    }
+//    public List<List<Integer>> subsets(int[] nums) {
+//
+//
+//
+//        return list;
+//    }
 }
