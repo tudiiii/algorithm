be97a183bca7d45bc0e37bc1e1fe274ebd254565
tudy <jhh0207@hanmail.net>
2021-07-02 18:59:12 +0900

    78. Subsets 문제&테스트케이스 생성

--- leetcode/src/Subsets/Leetcode_78.java
+++ leetcode/src/Subsets/Leetcode_78.java
@@ -1,10 +1,13 @@
 package Subsets;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Leetcode_78 {
-//    public List<List<Integer>> subsets(int[] nums) {
-//
-//
-//
-//        return list;
-//    }
+    public List<List<Integer>> subsets(int[] nums) {
+
+        List<List<Integer>> list = new ArrayList<>();
+
+        return list;
+    }
 }
