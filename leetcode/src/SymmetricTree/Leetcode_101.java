378a49f0354ba1fd3c22cdb2d9420a22c15366a4
tudy <jhh0207@hanmail.net>
2021-07-14 22:17:31 +0900

    Leetcode 39. Combination Sum 문제

---
+++ leetcode/src/SymmetricTree/Leetcode_101.java
@@ -0,0 +1,23 @@
+package SymmetricTree;
+
+//Definition for a binary tree node.
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+public class Leetcode_101 {
+    public boolean isSymmetric(TreeNode root) {
+
+        boolean result = true;
+        return result;
+    }
+}
