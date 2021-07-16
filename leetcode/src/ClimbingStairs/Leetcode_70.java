bfb9cafdd936c1bb7b79cb158efece587d7481dc
tudy <jhh0207@hanmail.net>
2021-07-16 21:20:32 +0900

    Leetcode 70. Climbing Stairs 문제 생성

---
+++ leetcode/src/ClimbingStairs/Leetcode_70.java
@@ -0,0 +1,8 @@
+package ClimbingStairs;
+
+public class Leetcode_70 {
+    public int climbStairs(int n) {
+
+        return 1;
+    }
+}
---
+++ leetcode/test/ClimbingStairs/Leetcode_70Test.java
@@ -0,0 +1,16 @@
+package ClimbingStairs;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Leetcode_70Test {
+    Leetcode_70 s = new Leetcode_70();
+
+    @Test
+    public void test(){
+        Assert.assertEquals(s.climbStairs(2),2);
+        Assert.assertEquals(s.climbStairs(3),3);
+    }
+}
