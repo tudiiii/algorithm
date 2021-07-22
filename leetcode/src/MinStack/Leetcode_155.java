7420c010030427cbe3fdf6c0f200993f5dbe7a7b
tudy <jhh0207@hanmail.net>
2021-07-22 22:47:49 +0900

    Leetcode 155. Min Stack 문제 생성

---
+++ leetcode/src/MinStack/Leetcode_155.java
@@ -0,0 +1,24 @@
+package MinStack;
+
+public class Leetcode_155 {
+    /** initialize your data structure here. */
+    public Leetcode_155() {
+
+    }
+
+    public void push(int val) {
+
+    }
+
+    public void pop() {
+
+    }
+
+    public int top() {
+        return 1;
+    }
+
+    public int getMin() {
+        return 1;
+    }
+}
---
+++ leetcode/test/MinStack/Leetcode_155Test.java
@@ -0,0 +1,14 @@
+package MinStack;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Leetcode_155Test {
+    Leetcode_155 s = new Leetcode_155();
+
+    @Test
+    public void test(){
+
+    }
+}
