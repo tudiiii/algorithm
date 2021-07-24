9ca6a4997d94af29ed04127a9da811fab337e9da
tudy <jhh0207@hanmail.net>
2021-07-24 23:24:48 +0900

    Leetcode 141. Linked List Cycle

---
+++ leetcode/src/LinkedListCycle/Leetcode_141.java
@@ -0,0 +1,17 @@
+package LinkedListCycle;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+
+public class Leetcode_141 {
+    public boolean hasCycle(ListNode head) {
+        return true;
+    }
+}
