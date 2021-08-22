43f5084d362645e1cb709aff1c46d9db1ec10293
tudy <jhh0207@hanmail.net>
2021-08-22 22:25:06 +0900

    Programmers 82612. 부족한 금액 계산하기

---
+++ programmers/src/부족한금액계산하기/Programmers_82612.java
@@ -0,0 +1,9 @@
+package 부족한금액계산하기;
+
+public class Programmers_82612 {
+    public long solution(int price, int money, int count) {
+        long answer = -1;
+
+        return answer;
+    }
+}
---
+++ programmers/test/부족한금액계산하기/Programmers_82612Test.java
@@ -0,0 +1,15 @@
+package 부족한금액계산하기;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Programmers_82612Test {
+    Programmers_82612 s  = new Programmers_82612();
+
+    @Test
+    public void test(){
+        assertEquals(s.solution(3,20,4),10);
+    }
+
+}
