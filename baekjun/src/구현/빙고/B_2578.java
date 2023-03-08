1d470eed438f08372ffab497dcc9163d88efb57a
tudiiii <jhh0207@hanmail.net>
2023-03-08 12:04:45 +0900

    Algorithm : 백준 ( 빙고, 스위치켜고끄기 )

--- baekjun/src/구현/빙고/B_2578.java
+++ baekjun/src/구현/빙고/B_2578.java
@@ -1,2 +1,113 @@
-package 구현.빙고;public class B_2578 {
+package 구현.빙고;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_2578 {
+
+    static int[][] map = new int[5][5];
+    static boolean bingo = false;
+    static int count;
+    static int say=0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        // 빙고 입력
+        for (int i = 0; i < 5; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 5; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < 5; j++) {
+                int num = Integer.parseInt(st.nextToken());
+
+                bingo(num);
+            }
+        }
+
+    }
+
+    private static void bingo(int num) {
+
+        say++;
+
+        for(int i = 0 ; i < 5 ; i++) {
+            for(int j = 0 ; j < 5 ; j++) {
+                if(map[i][j] == num) {
+                    map[i][j] = 0;
+                }
+            }
+        }
+
+        bingoCheck();
+
+        if(bingo){
+            System.out.println(say);
+            System.exit(0);
+        }
+
+        count=0;
+    }
+
+    private static void bingoCheck() {
+        rightCheck();
+        leftCheck();
+        rightCrossCheck();
+        leftCrossCheck();
+
+        if(count>=3) bingo = true;
+    }
+
+    private static void rightCheck(){
+        for(int i = 0 ; i < 5 ; i++) {
+            int bingoCount = 0;
+
+            for(int j = 0 ; j < 5 ;  j++) {
+                if(map[i][j]==0) bingoCount++;
+            }
+
+            if(bingoCount == 5) count++;
+        }
+    }
+
+    private static void leftCheck(){
+        for(int i = 0 ; i < 5 ; i++) {
+            int bingoCount = 0;
+
+            for(int j = 0 ; j < 5 ;  j++) {
+                if(map[j][i]==0) bingoCount++;
+            }
+
+            if(bingoCount == 5) count++;
+        }
+    }
+
+    private static void rightCrossCheck() {
+        int bingoCount = 0;
+
+        for (int i = 0; i < 5; i++) {
+            if(map[i][i]==0) bingoCount++;
+        }
+
+        if(bingoCount == 5) count++;
+    }
+
+    private static void leftCrossCheck() {
+        int bingoCount = 0;
+
+        for (int i = 0; i < 5; i++) {
+            if(map[i][4-i]==0) bingoCount++;
+        }
+
+        if(bingoCount == 5) count++;
+    }
 }
--- baekjun/src/구현/스위치켜고끄기/B_1244.java
+++ baekjun/src/구현/스위치켜고끄기/B_1244.java
@@ -1,2 +1,80 @@
-package 구현.스위치켜고끄기;public class B_1244 {
+package 구현.스위치켜고끄기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_1244 {
+
+    static int N;
+    static boolean[] bulb;
+    static int count;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        bulb = new boolean[N+1];
+        for (int i = 1; i <= N; i++) {
+            bulb[i] = !"1".equals(st.nextToken());
+        }
+
+        count = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < count; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+
+            if(s==1) boy(m);
+            else if(s==2) girl(m);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        int cnt =0;
+
+        for (int i = 1; i <= N; i++) {
+
+            if(i!=1 && cnt%20==0) sb.append(System.getProperty("line.separator"));
+
+            if (bulb[i])
+                sb.append("0").append(' ');
+            else
+                sb.append("1").append(' ');
+
+            cnt++;
+        }
+
+        System.out.println(sb);
+
+
+    }
+    
+    private static void boy(int num){
+        for (int i = num; i <= N; i+=num) {
+            bulb[i] = !bulb[i];
+        }
+    }
+    
+    private static void girl(int num){
+
+        bulb[num] = !bulb[num];
+
+        int left = num - 1;
+        int right = num + 1;
+
+        while(left>0 && right<=N){
+            if( bulb[left] == bulb[right] ){
+                bulb[left] = !bulb[left];
+                bulb[right] = !bulb[right];
+                left -= 1;
+                right += 1;
+            }else{
+                break;
+            }
+
+        }
+    }
 }
