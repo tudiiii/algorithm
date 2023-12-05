0924e5cbf89b154ee986271958195e599dd7fab9
tudiiii <jhh0207@hanmail.net>
2023-12-05 16:12:31 +0900

    Algorithm : 프로그래머스 ( 카펫 )

--- programmers/src/카펫/Programmers_42842.java
+++ programmers/src/카펫/Programmers_42842.java
@@ -1,17 +1,17 @@
 /*
-테스트 1 〉	통과 (0.03ms, 54MB)
-테스트 2 〉	통과 (0.03ms, 51.7MB)
-테스트 3 〉	통과 (0.05ms, 52.6MB)
-테스트 4 〉	통과 (0.02ms, 53.6MB)
-테스트 5 〉	통과 (0.02ms, 52MB)
-테스트 6 〉	통과 (0.03ms, 52.8MB)
-테스트 7 〉	통과 (0.05ms, 52.5MB)
-테스트 8 〉	통과 (0.03ms, 52.5MB)
-테스트 9 〉	통과 (0.05ms, 52.9MB)
-테스트 10 〉	통과 (0.04ms, 53.5MB)
-테스트 11 〉	통과 (0.03ms, 53.7MB)
-테스트 12 〉	통과 (0.01ms, 52.8MB)
-테스트 13 〉	통과 (0.03ms, 51.9MB)
+테스트 1 〉	통과 (0.01ms, 71.2MB)
+테스트 2 〉	통과 (0.03ms, 75.8MB)
+테스트 3 〉	통과 (0.08ms, 77.3MB)
+테스트 4 〉	통과 (0.02ms, 75.3MB)
+테스트 5 〉	통과 (0.02ms, 77.4MB)
+테스트 6 〉	통과 (0.03ms, 78.4MB)
+테스트 7 〉	통과 (0.06ms, 72.5MB)
+테스트 8 〉	통과 (0.07ms, 74.9MB)
+테스트 9 〉	통과 (0.07ms, 73.1MB)
+테스트 10 〉	통과 (0.09ms, 77.2MB)
+테스트 11 〉	통과 (0.01ms, 77.1MB)
+테스트 12 〉	통과 (0.02ms, 78.3MB)
+테스트 13 〉	통과 (0.02ms, 77.5MB)
  */
 
 package 카펫;
@@ -19,19 +19,19 @@ package 카펫;
 public class Programmers_42842 {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        int sum = (brown + 4) / 2; // 가로와 세로의 합
-        int width = 3; // 세로
-        int depth = sum - width; // 가로
 
-        while(depth >= 3 && depth >= width) { // 조건
+        for (int i = 3; i < brown + yellow; i++) {
+            int width = (brown + yellow) / i;
+            boolean isPrime = (brown + yellow) % i == 0;
+            if (isPrime && width >= i) {
+                int yellowTmp = (width - 2) * (i - 2);
 
-            if((depth - 2) * (width - 2) == yellow){
-                answer[0] = depth;
-                answer[1] = width;
-                break;
+                if (yellowTmp == yellow) {
+                    answer[0] = width;
+                    answer[1] = i;
+                    break;
+                }
             }
-
-            depth--; width++;
         }
 
         return answer;
--- programmers/test/카펫/Programmers_42842Test.java
+++ programmers/test/카펫/Programmers_42842Test.java
@@ -1,18 +1,17 @@
 package 카펫;
 
-import org.junit.Assert;
-import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class Programmers_42842Test {
     Programmers_42842 s = new Programmers_42842();
 
     @Test
     public void test(){
-        Assert.assertEquals(s.solution(10,2), new int[]{4,2});
-        Assert.assertEquals(s.solution(8,1), new int[]{3,3});
-        Assert.assertEquals(s.solution(24,24), new int[]{8,6});
+        assertArrayEquals(s.solution(10,2), new int[]{4,3});
+        assertArrayEquals(s.solution(8,1), new int[]{3,3});
+        assertArrayEquals(s.solution(24,24), new int[]{8,6});
     }
 
 }
